34b068c36744e37f7f54c95f837663f57f58406a
Glitch (slate-project) <none>
2018-07-03 17:34:06 +0000

    Initial Code Upload from Github

---
+++ script.js
@@ -0,0 +1,13 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+// prints "hi" in the browser's dev tools console
+/* Open */
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+/* Close */
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
